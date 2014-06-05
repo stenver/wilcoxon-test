04e85320c6af6c9ec26ed81b19f0310053ab8b43
stenver <stenver1010@gmail.com>
2014-06-05 17:56:00 +0000

    Added a new R test

--- RcppWilcoxonTest/test.r
+++ RcppWilcoxonTest/test.r
@@ -2,10 +2,7 @@
 first <- c(125, 115, 130, 140, 140, 115, 140, 125, 140, 135)
 second <- c(110, 122, 125, 120, 140, 124, 123, 137, 135, 145)
 
-t1 <- wilcox.test(first, second)
-
 ma <-  matrix(c(first, second), 2, 10)
-
 
 library(RcppWilcoxonTest)
 
