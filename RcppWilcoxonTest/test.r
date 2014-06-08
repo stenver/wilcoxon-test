24cf97cee89156fae7554d89d9153dc789644cab
stenver <stenver1010@gmail.com>
2014-06-08 16:55:05 +0000

    Improved R tests

--- RcppWilcoxonTest/test.r
+++ RcppWilcoxonTest/test.r
@@ -1,9 +1,18 @@
+x <- c(125, 115, 130, 140, 140, 115, 140, 125, 140, 135)
+y <- c(110, 122, 125, 120, 140, 124, 123, 137, 135, 145)
+ma <-  matrix(c(x, y), 2, 10)
 
-first <- c(125, 115, 130, 140, 140, 115, 140, 125, 140, 135)
-second <- c(110, 122, 125, 120, 140, 124, 123, 137, 135, 145)
+cat("R wilcoxon: ", wilcox.test(x, y, paired = TRUE, alternative = "greater")$p.value, " and RcppWilcoxon: ", RcppWilcoxonTest::WilxTest(ma, testIndexes = 0, controlIndexes = 1), "\n")
 
-ma <-  matrix(c(first, second), 2, 10)
+x <- c(125, 115, 140, 115, 140, 125, 140, 135)
+y <- c(110, 122, 120, 124, 123, 137, 135, 145)
+ma <-  matrix(c(x, y), 2, 8)
 
-library(RcppWilcoxonTest)
+cat("R wilcoxon: ", wilcox.test(x, y, paired = TRUE, alternative = "greater")$p.value, " and RcppWilcoxon: ", RcppWilcoxonTest::WilxTest(ma, testIndexes = 0, controlIndexes = 1), "\n")
 
-t2 <- RcppWilcoxonTest::WilxTest(ma, 0, 1)
+############################################
+x = c(3, 7, 12)
+y = c(1, 3, 5)
+ma = rbind(x, y)
+cat("R wilcoxon: ", wilcox.test(x, y, paired = TRUE, alternative = "greater")$p.value, " and RcppWilcoxon: ", RcppWilcoxonTest::WilxTest(ma, testIndexes = 0, controlIndexes = 1), "\n")
+
