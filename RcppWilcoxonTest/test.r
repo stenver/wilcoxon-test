1a4e906eb30b451c496e2aa374d1f690d50a7f43
stenver <stenver1010@gmail.com>
2014-06-12 09:15:21 +0000

    Improved the tests

--- RcppWilcoxonTest/test.r
+++ RcppWilcoxonTest/test.r
@@ -2,17 +2,50 @@ x <- c(125, 115, 130, 140, 140, 115, 140, 125, 140, 135)
 y <- c(110, 122, 125, 120, 140, 124, 123, 137, 135, 145)
 ma <-  matrix(c(x, y), 2, 10)
 
-cat("R wilcoxon: ", wilcox.test(x, y, paired = TRUE, alternative = "greater")$p.value, " and RcppWilcoxon: ", RcppWilcoxonTest::WilxTest(ma, testIndexes = 0, controlIndexes = 1), "\n")
+wilxResult = wilcox.test(x, y, exact = TRUE, paired = TRUE, alternative = "greater")$p.value
+rcppResult = RcppWilcoxonTest::WilxTest(ma, testIndexes = 0, controlIndexes = 1)
+cat("R wilcoxon: ", wilxResult, " and RcppWilcoxon: ", rcppResult,  "\n")
+
+###########################################
 
 x <- c(125, 115, 140, 115, 140, 125, 140, 135)
 y <- c(110, 122, 120, 124, 123, 137, 135, 145)
 ma <-  matrix(c(x, y), 2, 8)
-
-cat("R wilcoxon: ", wilcox.test(x, y, paired = TRUE, alternative = "greater")$p.value, " and RcppWilcoxon: ", RcppWilcoxonTest::WilxTest(ma, testIndexes = 0, controlIndexes = 1), "\n")
+wilxResult = wilcox.test(x, y, paired = TRUE, alternative = "greater")$p.value
+rcppResult = RcppWilcoxonTest::WilxTest(ma, testIndexes = 0, controlIndexes = 1)
+cat("R wilcoxon: ", wilxResult, " and RcppWilcoxon: ", rcppResult,  "\n")
 
 ############################################
+
 x = c(3, 7, 12)
 y = c(1, 3, 5)
 ma = rbind(x, y)
-cat("R wilcoxon: ", wilcox.test(x, y, paired = TRUE, alternative = "greater")$p.value, " and RcppWilcoxon: ", RcppWilcoxonTest::WilxTest(ma, testIndexes = 0, controlIndexes = 1), "\n")
+wilxResult = wilcox.test(x, y, paired = TRUE, alternative = "greater")$p.value
+rcppResult = RcppWilcoxonTest::WilxTest(ma, testIndexes = 0, controlIndexes = 1)
+cat("R wilcoxon: ", wilxResult, " and RcppWilcoxon: ", rcppResult,  "\n")
 
+###########################################
+
+#p = 120
+#a = rnorm(p)
+#b = rnorm(p) + 2
+#n = 20000
+#pv = rep(NA, n)
+#t1 = proc.time()[3]
+#for(i in 1:n){
+#  pv[i] = wilcox.test(a, b, paired = TRUE, exact = TRUE)$p.value
+#}
+#t2 = proc.time()[3]
+#t2 - t1
+#
+#ma = rbind(a, b)
+#le = rbind(a, b)
+#for(i in 1:n){
+#  le = rbind(le, ma)
+#}
+#
+#t1 = proc.time()[3]
+#RcppWilcoxonTest::WilxTest(le, c(0:19999), c(20000:39999))
+#t2 = proc.time()[3]
+#t2-t1
+#cat("lols")
